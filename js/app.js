8eb4a9c2cedff2eaa3fa8f533def795575f16750
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-14 10:48:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 fetch('../database/filmes.json')
-.then(r => r.json())
+.then(r=>r.json())
 .then(filmes=>{
+
+document.getElementById("totalFilmes").innerHTML=filmes.length;
 
 let html='';
 
 filmes.forEach(f=>{
 
 html+=`
-
 <tr>
 
 <td>${f.id}</td>
@@ -16,24 +17,11 @@ html+=`
 
 <td>${f.ano}</td>
 
-<td>
-
-<button>Editar</button>
-
-</td>
-
 </tr>
-
 `;
 
 });
 
-document.getElementById('filmes').innerHTML=html;
+document.getElementById("filmes").innerHTML=html;
 
 });
-
-document.getElementById("novoFilme").onclick=function(){
-
-alert("Em breve teremos o cadastro de filmes!");
-
-};
