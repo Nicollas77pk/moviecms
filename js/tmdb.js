f0b85121b34115ef260b913b5e1ee0b264c23aec
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-14 12:43:56 -0300

    Update tmdb.js

--- js/tmdb.js
+++ js/tmdb.js
@@ -4,9 +4,9 @@ async function buscarFilme(){
 let nomeFilme = document.getElementById("nomeFilme").value;
 
 
-if(nomeFilme.trim() === ""){
+if(nomeFilme.trim()==""){
 
-alert("Digite o nome de um filme");
+alert("Digite um filme");
 
 return;
 
@@ -14,8 +14,7 @@ return;
 
 
 
-let url = 
-`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=pt-BR&query=${nomeFilme}`;
+let url = `https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&language=pt-BR&query=${nomeFilme}`;
 
 
 
@@ -29,28 +28,113 @@ let dados = await resposta.json();
 
 
 
-console.log(dados);
+let filmes = dados.results;
 
 
 
-document.getElementById("resultadoBusca").innerHTML = `
+if(filmes.length === 0){
 
-<h3>Resultados encontrados:</h3>
+document.getElementById("resultadoBusca").innerHTML =
+`
+<p>Nenhum filme encontrado.</p>
+`;
 
-<p>${dados.results.length} filmes encontrados</p>
+return;
+
+}
+
+
+
+let html = "";
+
+
+
+filmes.slice(0,5).forEach(filme => {
+
+
+
+let imagem = filme.poster_path 
+? 
+`https://image.tmdb.org/t/p/w300${filme.poster_path}`
+:
+"";
+
+
+let ano = filme.release_date 
+? 
+filme.release_date.substring(0,4)
+:
+"Não informado";
+
+
+
+html += `
+
+<div class="resultado-filme">
+
+
+<img src="${imagem}" width="150">
+
+
+<div>
+
+
+<h3>${filme.title}</h3>
+
+
+<p><strong>Ano:</strong> ${ano}</p>
+
+
+<p>
+<strong>Sinopse:</strong><br>
+${filme.overview}
+</p>
+
+
+<button onclick="selecionarFilme(${filme.id})">
+
+Selecionar
+
+</button>
+
+
+</div>
+
+
+</div>
+
 
 `;
 
 
-}
 
-catch(erro){
+});
+
+
+
+document.getElementById("resultadoBusca").innerHTML = html;
+
+
+
+}catch(erro){
+
 
 console.log(erro);
 
-alert("Erro ao consultar TMDB");
+alert("Erro ao buscar filmes");
+
 
 }
+
+
+}
+
+
+
+function selecionarFilme(id){
+
+
+alert("Filme selecionado ID: "+id);
 
 
 }
