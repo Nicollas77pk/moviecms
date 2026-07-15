9a6d849b33b0c1c451f916da7aa3fdc69bab83f9
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-15 10:47:12 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,13 @@
+async function api(endpoint){
 
+    const url =
+        `${CONFIG.API_URL}${endpoint}` +
+        `${endpoint.includes("?") ? "&" : "?"}` +
+        `api_key=${CONFIG.API_KEY}` +
+        `&language=${CONFIG.LANGUAGE}`;
+
+    const resposta = await fetch(url);
+
+    return await resposta.json();
+
+}
