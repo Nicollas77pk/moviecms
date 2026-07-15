7587d705b0f4070eed3931d4d855a8d827fc6f36
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-15 10:50:38 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,19 @@
+document.addEventListener("DOMContentLoaded", iniciarHome);
 
+async function iniciarHome(){
+
+    carregarFilmes();
+
+}
+
+async function carregarFilmes(){
+
+    const dados = await api("/movie/popular");
+
+    renderizarCards(
+        "carrossel-filmes",
+        dados.results,
+        "movie"
+    );
+
+}
