1d79219ce697e06fcbeb3432c9e8cf01ec25a940
futebol ao vivo <91692892+Nicollas77pk@users.noreply.github.com>
2026-07-15 12:22:06 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,18 +2,27 @@ document.addEventListener("DOMContentLoaded", iniciarHome);
 
 async function iniciarHome(){
 
-    carregarFilmes();
+    await carregarBanner();
+
+    await carregarFilmes();
 
 }
 
-async function carregarFilmes(){
+async function carregarBanner(){
 
-    const dados = await api("/movie/popular");
+    const dados = await api("/trending/all/day");
 
-    renderizarCards(
-        "carrossel-filmes",
-        dados.results,
-        "movie"
-    );
+    const destaque = dados.results[0];
+
+    if(!destaque) return;
+
+    document.getElementById("hero").style.backgroundImage =
+        `url(${CONFIG.IMAGE_BACKDROP}${destaque.backdrop_path})`;
+
+    document.getElementById("hero-title").textContent =
+        destaque.title || destaque.name;
+
+    document.getElementById("hero-description").textContent =
+        destaque.overview;
 
 }
